669eb23e2f9f7e007e69abfd366c6690498adfba
Richard Oliveri <roliveri@redhat.com>
2016-12-15 13:56:12 -0500

    Restructured nativefs module.

--- lib/virtfs/nativefs/thick.rb
+++ lib/virtfs/nativefs/thick.rb
@@ -1,3 +1,24 @@
-require_relative 'thick/nativefs_base'
-require_relative 'thick/nativefs_file_class'
-require_relative 'thick/nativefs_dir_class'
+require_relative 'thick/file_class_methods'
+require_relative 'thick/dir_class_methods'
+
+module VirtFS::NativeFS # rubocop:disable Style/ClassAndModuleChildren
+  class Thick
+    attr_accessor :mount_point, :name
+
+    include FileClassMethods
+    include DirClassMethods
+
+    def initialize
+      @mount_point  = nil
+      @name         = self.class.name
+    end
+
+    def thin_interface?
+      false
+    end
+
+    def umount
+      @mount_point = nil
+    end
+  end
+end
